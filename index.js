5acaa9752250e2aa1f9207b33bf7d9edc2a16ec4
sabaree-88 <sabareesh887@gmail.com>
2024-08-19 11:31:21 +0530

    initial

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
-  console.log(`Server is running http://localhost:${PORT}`);
+  console.log(`Server is running.`);
 });
